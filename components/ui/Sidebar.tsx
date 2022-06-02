4b43c2ecab7fa9bf6e11a8973c2c18f584b1d41c
Jose Domenico <domenicodiaz.joseangel@gmail.com>
2022-06-02 16:18:31 -0700

    feat(formatters): se separaron formatters y se reviso todos los lugares donde se usaban

---
+++ components/ui/Sidebar.tsx
@@ -0,0 +1,55 @@
+import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
+import MailOutlinedOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
+import {
+  Drawer,
+  Box,
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+} from '@mui/material';
+
+const menuItems: string[] = ['Inbox', 'Starred', 'Send Email', 'Drafts'];
+
+export const Sidebar = () => {
+  return (
+    <Drawer anchor="left" open={true} onClose={() => console.log('cerrado')}>
+      <Box sx={{ width: 250 }}>
+        <Box sx={{ padding: '5px 10px' }}>
+          <Typography variant="h4">Menu</Typography>
+        </Box>
+        <List>
+          {menuItems.map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 ? (
+                  <InboxOutlinedIcon />
+                ) : (
+                  <MailOutlinedOutlinedIcon />
+                )}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {menuItems.map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 ? (
+                  <InboxOutlinedIcon />
+                ) : (
+                  <MailOutlinedOutlinedIcon />
+                )}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
+};
